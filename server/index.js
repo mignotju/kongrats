a886eb9ca86f64465bf38baf3563a56cacd4793a
kevin <kevinj@theodo.fr>
2016-11-20 23:26:34 +0100

    Marmotte

---
+++ server/index.js
@@ -0,0 +1,17 @@
+var fs = require('fs')
+var parse = require('csv-parse')
+
+module.exports = function(string) {
+
+  var readCSV = function(file) {
+    var parser = parse({delimiter: ';'}, function(err, data){
+      console.log(data);
+    });
+
+    fs.createReadStream(__dirname+'/test.csv').pipe(parser);
+  }
+
+  return {
+    readCSV: readCSV
+  }
+}
