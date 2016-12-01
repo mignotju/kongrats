ed21fc856f937b9b9d28f4a49ef3c04d2673c79c
Justine Mignot <mignot.justine@gmail.com>
2016-12-01 18:57:43 +0100

    correction des noms de variables

--- routes/data.js
+++ routes/data.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var server = require('../server/index');
+var Parser = require('../server/parser');
 
 /* GET data page. */
 router.get('/', function(req, res, next) {
-  var server2 = new server();
-  server2.readCSV(function(donnees) {
-    var donneesJson = {};
-    donneesJson.values = donnees;
-    res.render('data', { title : 'Retours', pageData: donneesJson });
+  var parser = new Parser();
+  parser.readCSV(function(feedbacks) {
+    var feedbacksJson = {};
+    feedbacksJson = feedbacks;
+    res.render('data', { title : 'Retours', pageData: feedbacksJson });
   });
 });
 
