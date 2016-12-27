600c99bda30551bb2a506f4b9c37b83eb28ffe25
Justine Mignot <mignot.justine@gmail.com>
2016-12-27 03:12:38 +0100

    petite modif pour faire apparaitre le nom de la matiere sur la page feedbacks

--- routes/feedbacks.js
+++ routes/feedbacks.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 var Parser = require('../server/parser');
 
-router.get('/:annee/:code', function(req, res, next) {
+router.get('/:annee/:code/:matiere', function(req, res, next) {
   var parser = new Parser('/../enseignements-'+req.params.annee+'.csv');
   parser.parserFeedbacks(req.params.code, function(feedbacks) {
     var feedbacksJson = {};
     feedbacksJson = feedbacks;
-    res.render('feedbacks', { title : 'Feedbacks', feedbacks: feedbacksJson });
+    res.render('feedbacks', { title : 'Feedbacks', feedbacks: feedbacksJson, matiere : req.params.matiere });
   });
 });
 
